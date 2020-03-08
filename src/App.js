a97dc6348ea4e3c2b7f64f692c78f6f25fda2b49
nishith97 <nishith.pinnawala@gmail.com>
2020-03-08 23:47:22 +0530

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="container">
+        <h1>Hello World!</h1>
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
+  );
+}
+
+export default App;
